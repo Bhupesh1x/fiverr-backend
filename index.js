df4fb1aec57421a447caa08af170a3591823f331
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-05-08 22:29:21 +0530

    Updated cors options configuration.

--- index.js
+++ index.js
@@ -27,7 +27,18 @@ connectToMongo();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+const whitelist = ["https://fiverr-backend-nitm.onrender.com"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use("/api/users", userRoutes);
 app.use("/api/gigs", gigRoutes);
